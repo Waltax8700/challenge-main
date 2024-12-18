45926e42283c49f5f28faa677306b336087f1488
Marco Sebastian Villanueva Murillo <90487999+Waltax8700@users.noreply.github.com>
2024-12-18 17:55:17 -0500

    Update gilded-rose.ts

--- game-02/app/gilded-rose.ts
+++ game-02/app/gilded-rose.ts
@@ -12,6 +12,10 @@ export class Item {
 
 export class GildedRose {
     items: Array<Item>;
+    Sulturas: string = 'Sulfuras, Hand of Ragnaros';
+    AgedBrie: string = 'Aged Brie';
+    Backstage: string = 'Backstage passes to a TAFKAL80ETC concert';
+    Conjured: string = 'Conjured';
 
     constructor(items = [] as Array<Item>) {
         this.items = items;
@@ -19,49 +23,44 @@ export class GildedRose {
 
     updateQuality() {
         for (let i = 0; i < this.items.length; i++) {
-            if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if (this.items[i].quality > 0) {
-                    if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                        this.items[i].quality = this.items[i].quality - 1
-                    }
-                }
-            } else {
-                if (this.items[i].quality < 50) {
-                    this.items[i].quality = this.items[i].quality + 1
-                    if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (this.items[i].sellIn < 11) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1
-                            }
-                        }
-                        if (this.items[i].sellIn < 6) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1
-                            }
-                        }
-                    }
-                }
+            let item = this.items[i];
+
+            if (item.name === this.Sulturas)
+                continue;
+            
+            item.sellIn -= 1;
+
+            let qualityChange = 1;
+
+            if (item.name === this.Conjured) 
+                qualityChange = 2;
+
+            if (item.name !== this.AgedBrie && item.name !== this.Backstage) {
+                if (item.quality > 0)
+                    item.quality -= qualityChange;
             }
-            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].sellIn = this.items[i].sellIn - 1;
-            }
-            if (this.items[i].sellIn < 0) {
-                if (this.items[i].name != 'Aged Brie') {
-                    if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (this.items[i].quality > 0) {
-                            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                                this.items[i].quality = this.items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        this.items[i].quality = this.items[i].quality - this.items[i].quality
-                    }
-                } else {
-                    if (this.items[i].quality < 50) {
-                        this.items[i].quality = this.items[i].quality + 1
+            else{
+                if (item.quality < 50) {
+                    item.quality += qualityChange;
+    
+                    if (item.name === this.Backstage) {
+                        if (item.sellIn < 6) 
+                            item.quality += 1;
+                        if (item.sellIn < 11) 
+                            item.quality += 1;
                     }
                 }
             }
+            if (item.sellIn < 0) {
+                if (item.name === this.AgedBrie && item.quality < 50)
+                    item.quality += 1;
+                else if (item.name === this.Backstage)
+                    item.quality = 0;
+                else if (item.quality > 0)
+                    item.quality -= qualityChange;
+            }
+
+            item.quality = Math.min(Math.max(item.quality, 0), 50);
         }
 
         return this.items;
